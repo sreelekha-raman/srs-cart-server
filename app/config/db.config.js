f284facb1b195e30e54657862c8801837746737b
Sowpu <sowparnika1703@gmail.com>
2020-11-11 21:02:17 +0530

    completed

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "sowpu@2000",
+    DB: "srscart",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
---
+++ app/controller/product.controller.js
@@ -0,0 +1,35 @@
+const db = require("../models");
+const Product = db.products;
+const Op = db.Sequelize.Op;
+
+
+exports.create = (req, res) => {
+    const ProductId=req.body.PId;
+    const ProductName=req.body.Pname;
+    console.log(ProductId ,ProductName);
+
+    if (!ProductId) {
+        res.status(400).send({
+          message: "Content can not be empty!"
+        });
+        return;
+      }
+    
+     
+      const product = {
+        id: ProductId,
+        name: ProductName,
+      };
+    
+      
+      Product.create(product)
+        .then(data => {
+          res.send(data);
+        })
+        .catch(err => {
+          res.status(500).send({
+            message:
+              err.message || "Some error occurred while creating the product."
+          });
+        });
+};
